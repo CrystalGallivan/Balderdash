bc05113126111464d6ad458ece519425cfcfd250
Chelsey Jetsel <crjetsel@gmail.com>
2019-05-31 19:06:36 -0600

    adding icons

--- FrontEnd/app/models/Comment.js
+++ FrontEnd/app/models/Comment.js
@@ -7,7 +7,7 @@ export default class Comment {
     return `
               <div class="col-12">
                 <div class="row justify-content-left">
-                  <div class="col-1 bg-success rounded-circle">^</div>
+                  <div class="col-1rounded-circle"></div>
                   <div class="col-1 bg-danger rounded-circle">V</div>
                   <div class="col-9">${this.comment}</div>`
   }
